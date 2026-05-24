e5890e8b975de57ea6b721b81019bf0cef0fac2f
valentin <valentin.arias@mi.unc.edu.ar>
2026-05-23 22:07:44 -0300

    fix: remove wrong file

--- include/LPC17xx.h
+++
@@ -1,8 +0,0 @@
-#ifndef LPC17XX_H
-#define LPC17XX_H
-
-/* Minimal stub for LPC17xx MCU header to satisfy local builds. */
-
-/* Add real register definitions or include the SDK header here when available. */
-
-#endif /* LPC17XX_H */
